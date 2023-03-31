19b2cf03f75f42b195f854c527ef8ccda7770a39
Susmeta Akter Mim <susmetaaktermim@gmail.com>
2023-03-31 23:15:48 +0600

    responsive added

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -3,23 +3,33 @@ import React from 'react';
 const Blog = (props) => {
     const {blogCoverImage, authorImage, authorName, publishDate,bookMark, readTime, blogTitle,aboutRead} = props.blog;
     return (
-        <div className='mt-5 ml-40'>
+        <div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
+        <div className='mt-5 ml-48 mb-5'>
             <p><img src={blogCoverImage}/></p>
-            <div className='flex gap-80'>
-                <div className='flex'>
-                  <p><img className='h-10 w-10 rounded-full' src={authorImage}/></p>
-                    <div>
-                       <p className='ml-2 font-bold'>{authorName}</p> 
-                       <p>{publishDate}</p>     
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mt-3'>
+                <div>
+                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
+                        <p><img className='h-10 w-10 rounded-full' src={authorImage}/></p>
+                        <div className=' -ml-16'>
+                        <p className='ml-2 font-bold'>{authorName}</p> 
+                        <p>{publishDate}</p>               
+                     </div>
                     </div>
+                    <h1 className='font-bold text-xl mt-3'>{blogTitle}</h1>
+                    <p className=' text-indigo-600 underline mb-3 mt-3'>{aboutRead} </p>
                 </div>
-                <div className='gap-1 flex'>
-                <p>{readTime}</p>
-                <p><img className='h-6 w-6' src={bookMark} /></p>
+                <div className='gap-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
+                    <p className=' w-28'>{readTime}</p>
+                    <p><img className='h-6 w-6' src={bookMark} /></p>
                 </div>
             </div>
-            <h1 className='font-bold text-xl'>{blogTitle}</h1>
-            <p>{aboutRead}</p>
+            <hr/>
+            </div>
+            <div>
+                <p className=' mx-28 w-52 mt-3'>Spent time on read : 177 min</p>
+            </div>
+        </div>
         </div>
     );
 };
