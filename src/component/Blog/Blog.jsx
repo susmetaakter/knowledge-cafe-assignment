0bdc072e93e2040823250fcfc115b0d5fb1022f6
Susmeta Akter Mim <susmetaaktermim@gmail.com>
2023-03-31 23:46:14 +0600

    sideCard added

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -4,14 +4,14 @@ const Blog = (props) => {
     const {blogCoverImage, authorImage, authorName, publishDate,bookMark, readTime, blogTitle,aboutRead} = props.blog;
     return (
         <div>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
+        <div className='flex grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
         <div className='mt-5 ml-48 mb-5'>
             <p><img src={blogCoverImage}/></p>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mt-3'>
+            <div className='flex grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mt-3'>
                 <div>
-                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
+                 <div className='flex grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
                         <p><img className='h-10 w-10 rounded-full' src={authorImage}/></p>
-                        <div className=' -ml-16'>
+                        <div>
                         <p className='ml-2 font-bold'>{authorName}</p> 
                         <p>{publishDate}</p>               
                      </div>
@@ -19,7 +19,7 @@ const Blog = (props) => {
                     <h1 className='font-bold text-xl mt-3'>{blogTitle}</h1>
                     <p className=' text-indigo-600 underline mb-3 mt-3'>{aboutRead} </p>
                 </div>
-                <div className='gap-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
+                <div className='gap-1 flex grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
                     <p className=' w-28'>{readTime}</p>
                     <p><img className='h-6 w-6' src={bookMark} /></p>
                 </div>
@@ -27,7 +27,7 @@ const Blog = (props) => {
             <hr/>
             </div>
             <div>
-                <p className=' mx-28 w-52 mt-3'>Spent time on read : 177 min</p>
+                <p className=' mx-48 w-52 mt-3'>Spent time on read : 177 min</p>
             </div>
         </div>
         </div>
