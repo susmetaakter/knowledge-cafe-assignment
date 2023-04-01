a2044977633748e75a9466c5732ca82cb41de823
Susmeta Akter Mim <susmetaaktermim@gmail.com>
2023-04-01 13:29:37 +0600

    another side cart added

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 const Blog = (props) => {
     const {blogCoverImage, authorImage, authorName, publishDate,bookMark, readTime, blogTitle,aboutRead} = props.blog;
+    const addToCart = (blogTitle) =>{
+        console.log('item added', blogTitle)
+    }
     return (
         <div>
-        <div className='flex grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
-        <div className='mt-5 ml-48 mb-5'>
-            <p><img src={blogCoverImage}/></p>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 ml-16'>
+        <div className='mt-5 mb-5'>
+            <img className='rounded' src={blogCoverImage}/>
             <div className='flex grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mt-3'>
                 <div>
                  <div className='flex grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
-                        <p><img className='h-10 w-10 rounded-full' src={authorImage}/></p>
+                        <img className='h-10 w-10 rounded-full' src={authorImage}/>
                         <div>
                         <p className='ml-2 font-bold'>{authorName}</p> 
                         <p>{publishDate}</p>               
@@ -21,13 +24,18 @@ const Blog = (props) => {
                 </div>
                 <div className='gap-1 flex grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
                     <p className=' w-28'>{readTime}</p>
-                    <p><img className='h-6 w-6' src={bookMark} /></p>
+                    <p onClick={() => addToCart(blogTitle)}><img className='h-6 w-6' src={bookMark} /></p>
                 </div>
             </div>
             <hr/>
             </div>
             <div>
-                <p className=' mx-48 w-52 mt-3'>Spent time on read : 177 min</p>
+            <div>
+                <p className=' w-60 mt-5 bg-violet-100 font-bold px-2 py-2 text-indigo-800 rounded ml-5'>Spent time on read : 177 min</p>
+            </div>
+            <div>
+            <p className=' bg-slate-100 px-2 py-2 font-bold rounded w-60 mt-5 ml-5'>Bookmarked Blogs:</p>
+            </div>
             </div>
         </div>
         </div>
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className=' mr-48 ml-48 mt-5'>
-          <div className='flex justify-between mb-3'>
+        <div className='mt-5 ml-16 mr-80'>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mb-3'>
+          <div> 
           <h1 className='text-xl font-bold'>Knowledge Cafe</h1>
-          <img className='h-10 w-10 rounded-full' src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" alt="" />
+          </div> 
+          <div>
+          <img className='h-10 w-10 rounded-full ml-96' src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60" alt="" />
+          </div>
           </div>
           <hr />
        </div>
